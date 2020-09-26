3b63a669cb58e4fde04f0a8b718a765043c88bcf
Rakin Bin Rabbani <35832569+Rakin7@users.noreply.github.com>
2020-09-26 22:42:20 +0600

    Add files via upload

---
+++ Extras/nRF_recieve_NodeMCU/nRF_recieve_NodeMCU.ino
@@ -0,0 +1,21 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+
+RF24 radio(2,15); // CE, CSN
+const byte address[6] = "00002";
+void setup() {
+  Serial.begin(115200);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+}
+void loop() {
+  if (radio.available()) {
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+  }
+}
